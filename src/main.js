f4c5aeb93ebe38081f4796a572e322c9c69532b9
R-thinking <Making.R.W@gmail.com>
2021-05-08 12:26:16 +0900

    "Category Bar & Search Bar Commit_01" Description: added Category Bar & Search Bar in case of mobile view. Those will be activated and inactivated with animation by each click event.

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@
 // Main
 {
     CreateNavigation();
+    AppearCategoryBar();
+    DisappearCategoryBar();
+    ControlSearchBar();
 }
 
 // Function
@@ -13,6 +16,7 @@ function CreateNavigation () {
     const header =document.querySelector('.header');
     const headerHeight = header.clientHeight;
     const membershipIcon = document.querySelectorAll('.header .membership i');
+
     let position =pageYOffset;
     document.addEventListener('scroll',onScroll,{passive:true});
     window.addEventListener('resize',onresize);
@@ -20,21 +24,25 @@ function CreateNavigation () {
     // listener
     // If user scroll down under the header
     function onScroll () {
+        const clientWidth = document.body.clientWidth;
         const scrollPosition = pageYOffset;
         position=scrollPosition;
+
         if (headerHeight<scrollPosition){
             if(header.className==='fix'){
                 return;
             }
             header.classList.add('fix');
         }
-        else if (window.outerWidth > 1100) {
+        else if (clientWidth > 1100) {
             header.classList.remove('fix');
         }
     }
     // if browser width is smaller than 1100px
     function onresize () {
-        if (window.outerWidth<=1100){
+        const clientWidth = document.body.clientWidth;
+
+        if (clientWidth<=1100){
             membershipIcon.forEach((icon)=>{
                 icon.classList.remove('fa-2x');
             })
@@ -57,6 +65,66 @@ function CreateNavigation () {
     }
 }
 
-// Mobile
+// Appear CategoryBar
+function AppearCategoryBar() {
+    const menuBtn=document.querySelector('.header .menu .moreBtn');
+    const categoryBar=document.querySelector('.categoryBar');
+    menuBtn.addEventListener('click',()=> {
+        categoryBar.classList.remove('disappear');
+        categoryBar.classList.add('appear');
+    });
+    
+}
 
+// Disappear CategoryBar
+function DisappearCategoryBar() {
+    const EscBtn = document.querySelector('.categoryBar .inner .categoryHeader .escBtn');
+    const categoryBar=document.querySelector('.categoryBar');
+    EscBtn.addEventListener('click',()=> {
+        categoryBar.classList.add('disappear');
+        setTimeout(()=>categoryBar.classList.remove('appear'),700);
+    });
 
+}
+
+// Control SearchBar
+function ControlSearchBar() {
+    const searchBtn = document.querySelector('.header .menu .searchBtn');
+    const searchBar = document.querySelector('.searchBar');
+    const escBtn = document.querySelector('.searchBar .escBtn');
+
+    // listener
+    // Search button click
+    searchBtn.addEventListener('click',()=> {
+        // Appear SearchBar
+        if(searchBar.classList.contains('disappear')){
+            searchBar.classList.remove('disappear');
+            searchBar.classList.add('appear');
+        }
+        // Disappear SearchBar
+        else if(searchBar.classList.contains('appear')){
+            searchBar.classList.add('disappear');
+            setTimeout(()=>searchBar.classList.remove('appear'),400);
+        }
+        // Appear SearchBar at first
+        else {
+            searchBar.classList.add('appear');
+        }
+    });
+
+    // Client width resizing (smaller than  720px)
+    window.addEventListener('resize',() => {
+        const clientWidth = document.body.clientWidth;
+        if(clientWidth>720){
+            searchBar.classList.add('disappear');
+            setTimeout(()=>searchBar.classList.remove('appear'),400);
+        }
+    });
+
+    // Esc button click
+    escBtn.addEventListener('click',()=> {
+        searchBar.classList.add('disappear');
+        setTimeout(()=>searchBar.classList.remove('appear'),400);
+    });
+
+}
